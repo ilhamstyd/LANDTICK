6db139019915ceb43919a353eb0319c5a71d2aae
unknown <ilhamsetyadji28@gmail.com>
2023-07-21 13:44:28 +0700

    styling list admin

--- client/src/component/Footer.jsx
+++ client/src/component/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function FooterBar() {
   return (
     <>
-      <div style={{ background: "linear-gradient(90deg, #EC7AB7 -0.6%, #EC7A7A 100%)", width: "100%", height: "80px", marginTop: "170px" }}></div>
+      <div style={{ background: "linear-gradient(90deg, #EC7AB7 -0.6%, #EC7A7A 100%)", width: "100%", height: "80px", marginTop: "200px" }}></div>
     </>
   );
 }
